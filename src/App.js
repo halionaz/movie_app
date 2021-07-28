035a53e944a1d6400dadfa128bff75fd2e85bea3
haSaJa <samhsj@naver.com>
2021-07-29 00:57:13 +0900

    Learning JSX & props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Magpie from './Magpie';
 
+let magpieList = [
+  {name : 'halion', gender : 'male', age : 18, rating : 3.5},
+  {name : 'Jason', gender : 'male', age : 24, rating : 2},
+  {name : 'JungSeungHyun', gender : 'female', age : 19, rating : 4.5}
+]
+
 function App() {
   return (
     <div className="App">
       <h1>Hello, React!</h1>
-      <Magpie />
+      {magpieList.map(person => {
+        return <Magpie key={person.name} name={person.name} gender={person.gender} age={person.age} rating={person.rating} />
+      })}
     </div>
   );
 }
--- src/Magpie.js
+++ src/Magpie.js
@@ -1,8 +1,22 @@
 
-function Magpie(){
+import PropTypes from 'prop-types';
+
+// 따로 설치한 npm 요소
+
+function Magpie(props){
     return(
-        <h3>I'm the magpie guy!</h3>
+        <div>
+            <h3>I'm {props.name}</h3>
+            <p>Peer Review : {props.rating}/5.0</p>
+        </div>
     )
 }
+
+Magpie.propTypes = {
+    name : PropTypes.string.isRequired,
+    rating : PropTypes.number.isRequired,
+    age : PropTypes.number,
+    gender : PropTypes.string
+};
 
 export default Magpie;
