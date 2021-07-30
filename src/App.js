b1fc0d188adef3825c0d0efe09f9f61da7eefd8e
haSaJa <samhsj@naver.com>
2021-07-30 23:51:47 +0900

    Learning State

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-import Magpie from './Magpie';
+import React from "react";
 
-let magpieList = [
-  {name : 'halion', gender : 'male', age : 18, rating : 3.5},
-  {name : 'Jason', gender : 'male', age : 24, rating : 2},
-  {name : 'JungSeungHyun', gender : 'female', age : 19, rating : 4.5}
-]
+//React.Component는 state를 가지고 있음
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello, React!</h1>
-      {magpieList.map(person => {
-        return <Magpie key={person.name} name={person.name} gender={person.gender} age={person.age} rating={person.rating} />
-      })}
+class App extends React.Component{
+  state = {
+    number : 0
+  }
+  add = () => {
+    this.setState(cur => ({ number : cur.number + 1 }))
+  }
+  minus = () => {
+    this.setState({ number : this.state.number - 1})
+  }
+  componentDidMount(){
+    console.log("방금 웹사이트가 처음으로 구현되었습니다.");
+  }
+  componentDidUpdate(){
+    console.log("방금 웹사이트가 업데이트 되었습니다.");
+  }
+  componentWillUnmount(){
+    console.log("웹사이트를 떠납니다! 안녕!");
+    // 제일 마지막에 실행되어 console log를 볼 순 없음
+  }
+  render(){
+    const {number} = this.state;
+    return <div>
+      <h1>I am Class Component : {this.state.number}</h1>
+      <h1>I am Class Component : {number}</h1>
+      <button onClick={this.add}>ADD</button>
+      <button onClick={this.minus}>MINUS</button>
     </div>
-  );
+  }
 }
 
 export default App;
