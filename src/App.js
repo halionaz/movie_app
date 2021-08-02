3e4fc0a926cf1895ce2eebdce8531d551b13d544
haSaJa <samhsj@naver.com>
2021-08-02 21:47:20 +0900

    Update Routing

--- src/App.js
+++ src/App.js
@@ -1,60 +1,14 @@
-import React from "react";
-import axios from "axios";
+import { HashRouter, Route } from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
+import Navigation from './components/Navigation';
 
-import Movie from './Movie';
-
-//React.Component는 state를 가지고 있음
-
-class App extends React.Component{
-  state = {
-    isLoading : true,
-    movies : []
-  }
-  getMovies = async () => {
-    const {
-      data : {
-        data : {movies}
-      }
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-    console.log(movies);
-    this.setState({movies : movies})
-  }
-  async componentDidMount(){
-    await this.getMovies();
-    this.setState({isLoading : false})
-    console.log("방금 웹사이트가 처음으로 구현되었습니다.");
-  }
-  componentDidUpdate(){
-    console.log("방금 웹사이트가 업데이트 되었습니다.");
-  }
-  componentWillUnmount(){
-    console.log("웹사이트를 떠납니다! 안녕!");
-    // 제일 마지막에 실행되어 console log를 볼 순 없음
-  }
-  render(){
-    const {isLoading, movies} = this.state;
-    return <section className="container">
-      {isLoading ? 
-      (<div className="loader">
-        <span className="loaderText">Loading...</span>
-      </div> )
-      : 
-      (<div className="movies">
-        {movies.map(movie=>{
-          return <Movie 
-            key={movie.id}
-            id={movie.id} 
-            year={movie.year} 
-            title={movie.title} 
-            summary = {movie.summary} 
-            poster = {movie.medium_cover_image} 
-            genres = {movie.genres}
-          />
-        })}
-      </div>)
-      }
-    </section>
-  }
+function App(){
+  return <HashRouter>
+    <Navigation />
+    <Route path="/" exact={true} component= {Home} />
+    <Route path="/about" component= {About} />
+  </HashRouter>
 }
 
 export default App;
---
+++ src/routes/About.js
@@ -0,0 +1,6 @@
+
+function About(){
+    return <span>영화를 사랑하는 halion이 만들었습니다.</span>
+}
+
+export default About;
